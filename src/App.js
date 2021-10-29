12d5987c3b16168dca47d4ea86919840a47d55ee
Marc Klefter <marc.klefter@edument.se>
2021-10-29 12:55:22 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useTransition,
 
-function App() {
+  Suspense
+} from 'react';
+
+import {
+  UserProfile
+} from './UserProfile';
+
+import {
+  Repos
+} from './Repos';
+
+import {
+  Followers
+} from './Followers';
+
+import {
+  BASE_URL
+} from './constants';
+
+// ...
+
+const users = [
+  'marcklefter',
+  'krawaller',
+  'masak',
+  'toshi38'
+];
+
+// ...
+
+export const App = () => (
+  <Suspense fallback={`Loading view...`}>
+    <AppInternal />
+  </Suspense>
+)
+
+const AppInternal = () => {
+  const [
+    currentUser,
+    setCurrentUser
+  ] = useState(null);
+
+  const [
+    showProfile,
+    setShowProfile
+  ] = useState(false);
+
+  const [isPending, startTransition] = useTransition();
+
+  const selectUser = user => {
+    setCurrentUser(user);
+
+    startTransition(() => {
+      setShowProfile(true);
+    });
+
+    // ...
+    // If the showProfile state update is not wrapped in a transition, the outer Suspense boundary will be triggered 
+    // and the "Loading view..." fallback shown.
+    // 
+    // setShowProfile(true);
+  };
+
+  const goBack = () => {
+    setCurrentUser(null);
+    setShowProfile(false);
+  };
+
+  // ...
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    !showProfile
+      ? users.map(user => (
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          key={user}
+          style={{
+            display: 'block',
+            cursor: 'pointer'
+          }}
+          onClick={() => selectUser(user)}
         >
-          Learn React
+          {user} {(isPending && currentUser === user) && '...'}
         </a>
-      </header>
-    </div>
-  );
-}
+      ))
+      : (
+        <>
+          <button onClick={goBack}>Back</button>
 
-export default App;
+          <div className="row" style={{ flexDirection: 'column' }}>
+            <div className="column">
+              <UserProfile endpoint={`${BASE_URL}/${currentUser}`} />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="column">
+              {/* this suspense boundary allows us to load repos "lazily", i.e. ensuring that rendering the 
+                critical UserProfile component is not delayed by the not-as-vital Repos component. */}
+              <Suspense fallback={`Loading repos...`}>
+                <Repos endpoint={`${BASE_URL}/${currentUser}/repos`} />
+              </Suspense>
+            </div>
+
+            <div className="column">
+              {/* this suspense boundary allows us to load followers "lazily", i.e. ensuring that rendering the 
+                critical UserProfile component is not delayed by the not-as-vital Followers component. */}
+              <Suspense fallback={`Loading followers...`}>
+                <Followers endpoint={`${BASE_URL}/${currentUser}/followers`} />
+              </Suspense>
+            </div>
+          </div>
+        </>
+      )
+  )
+}
